cf2937aa4eb58102a1e566a878bea23f3d673c99
cgodevs <kharol.go@gmail.com>
2026-05-03 18:51:42 -0300

    refactor: :recycle: Route resumes

--- fastapi_tests/app/routers/resumes.py
+++ fastapi_tests/app/routers/resumes.py
@@ -1,4 +1,4 @@
-from .. import analysis_utils as utils
+from .. import utils
 from fastapi import UploadFile, status, HTTPException, APIRouter
 
 router = APIRouter()
@@ -13,10 +13,14 @@ async def send_resume(file: UploadFile):
 async def candidate_resume(resume_id: str):
     db = utils.DatabaseManager()
     try:
-        resume_df = db.get_resume(resume_id)
-        resume_obj = resume_df.to_dict(orient="records")
+        resume_df = db.get_resume(resume_id).drop(columns=["position_embedding"])
     except Exception as e:
-        raise HTTPException(status_code=404, detail=f"Resume with id {resume_id} not found") from e
+        raise HTTPException(status_code=500, detail="Failed to fetch resume") from e
     finally:
         db.close_all()
+
+    resume_obj = resume_df.to_dict(orient="records")[0] if not resume_df.empty else {}
+    if resume_obj == {}:
+        raise HTTPException(status_code=404, detail=f"Resume with id {resume_id} not found")
+
     return resume_obj
